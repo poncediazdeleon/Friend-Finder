eea5dcef7a4d09ac2654947c680265310deefcd3
Michael Diaz <poncediazdeleon@gmail.com>
2019-04-10 22:20:21 -0600

    update

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+
+var userData = [ 
+
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Leonardo Da Vinci",
+    "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJBPZeBYcYf0Mt6d8CEk9tS8iZTW8C6DgQ_fB4F-4sb7AG7SSO",
+    "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+  },
+  {
+    "name":"Mona Lisa",
+    "photo":"https://cdn.britannica.com/s:300x500/24/189624-131-BAF1184D.jpg",
+    "scores":[
+        3,
+        2,
+        5,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
+// Export to API Route
+module.exports = userData;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,76 @@
+
+var userData = require("../data/friends");
+
+module.exports = function(app) {
+  
+  app.get("/api/friends", function(req, res) {
+    res.json(userData);
+  });
+
+  var comparisonUserTotalScore = 0;
+
+  var friendScores = [];
+
+  app.post("/api/friends", function(req, res) {
+
+    // Store current user scores in array.
+    var currentUserScores = req.body.scores;
+
+    console.log("Current user scores: " + currentUserScores);
+
+    // Determine the user's most compatible friend.
+    for (var i = 0; i < userData.length; i++) {
+
+      // Convert each user's results in to an array of numbers.
+      var comparisonUserScores = userData[i].scores;
+
+      // Find total difference between current user and each user.
+      comparisonUserTotalScore = calculateUserCompatibilityScore(currentUserScores, comparisonUserScores);
+
+      // Build up array of user compatibility scores.
+      friendScores.push(comparisonUserTotalScore);
+
+    }
+
+    console.log("Array of friend scores: " + friendScores);
+
+    var index = 0;
+    var value = friendScores[0];
+
+    for (var i = 0; i < friendScores.length; i++) {
+      console.log("Value of item in array: " + friendScores[i]);
+      if (friendScores[i] < value) {
+        value = friendScores[i];
+        index = i;
+      }
+    }
+
+    // OMG we are getting a best friend.
+    console.log("Best friend name: " + userData[index].name);
+
+    // Send best friend as a response so we can display in modal.
+    res.send(userData[index]);
+
+    // Push new user to user array.
+    userData.push(req.body);
+
+  });
+};
+
+var totalDifference = 0;
+
+// Find total difference between current user and another user.
+function calculateUserCompatibilityScore(currentUserScores, comparisonUserScores) {
+
+  // Reset the total difference counter each time function called.
+  totalDifference = 0;
+
+  for (var i = 0; i < currentUserScores.length; i++) {
+
+    totalDifference+=Math.abs(currentUserScores[i] - comparisonUserScores[i]);
+  }
+
+  console.log("Final total difference for friend: " + totalDifference);
+
+  return totalDifference;
+};
